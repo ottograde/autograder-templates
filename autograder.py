110e1b3b02ab372686d9d5492d0d98332a038735
Robert Scheidegger <robert_scheidegger@brown.edu>
2023-12-14 15:15:50 -0500

    feat: make autograder longer

--- autograder.py
+++ autograder.py
@@ -24,24 +24,27 @@ student_fibonacci = load_module_from_file("/student/fibonacci.py", "fibonacci")
 if student_fibonacci.fibonacci(1) == 1:
     print("Test 1 passed")
 
-time.sleep(1)
+time.sleep(2)
 
 if student_fibonacci.fibonacci(2) == 1:
     print("Test 2 passed")
 
-time.sleep(1)
+time.sleep(2)
 
 if student_fibonacci.fibonacci(3) == 2:
     print("Test 3 passed")
 
-time.sleep(1)
+time.sleep(2)
 
 if student_fibonacci.fibonacci(10) == 55:
     print("Test 4 passed")
 
-time.sleep(1)
+time.sleep(2)
 
 if student_fibonacci.fibonacci(20) == 6765:
     print("Test 5 passed")
 
 time.sleep(1)
+
+if student_fibonacci.fibonacci(30) == 832040:
+    print("Test 6 passed")
